cbde8f1ecab4c357b0273eb977100791728f09f6
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 21:02:27 -0400

    Agregar archivos de proyecto.

---
+++ Primo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(numPrimo(34));
+        }
+        public static bool numPrimo(int num)
+        {
+            bool u;
+            if (num % 2==0)
+            {
+                u = true;
+                return u;
+            }
+            else
+            {
+                u = false;
+                return u;
+            }
+        }
+    }
+}
